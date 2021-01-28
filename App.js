cea6d93d1a0866c89553c08934ce37b716c2c63d
tanayshah454 <tanay.shah.naren@gmail.com>
2021-01-28 18:20:36 +0530

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {Header} from 'react-native-elements'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  }
+});
